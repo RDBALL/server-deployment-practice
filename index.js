40afb32c3ed4658544606308e43930e835779655
Robert Ball <bobball89@gmail.com>
2022-09-19 21:44:14 +0000

    sserver functionality passed npm test

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 'use strict';
 
-const express = require('express');
-const notFound = require('./error-handlers/404');
-const errorHandler = require('./error-handlers/500');
+const { start } = require('./src/app');
 
-console.log('The server is up and running');
-
-const app = express();
-
-app.get('*', (req, res, next) => {
-  res.send('This works as a catch all for any route');
-});
-
-app.listen(3002);
+start();
--- src/error-handlers/404.js
+++ src/error-handlers/404.js
@@ -1,9 +1,10 @@
 'use strict';
 
 module.exports = (req, res, next) => {
+  console.log(req);
   res.status(404).send({
     error: 404,
-    route: req.path,
-    message: 'Not Found',
+    route: req.originalURL,
+    message: 'Route not found',
   });
 };
